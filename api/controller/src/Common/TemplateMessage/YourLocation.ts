1cebcaf369307d72797c3523872a5fc93ba8029d
sasaki <skyo.97.admj@gmail.com>
2021-06-20 13:55:57 +0900

    [LINE] Create an error message when the condition is not met

--- api/controller/src/Common/TemplateMessage/YourLocation.ts
+++ api/controller/src/Common/TemplateMessage/YourLocation.ts
@@ -1,7 +1,7 @@
 // Load the package
 import { TemplateMessage } from '@line/bot-sdk';
 
-export const SendYourLocation = (): TemplateMessage => {
+export const yourLocationTemplate = (): TemplateMessage => {
   return {
     type: 'template',
     altText: '現在地を送ってください！',
