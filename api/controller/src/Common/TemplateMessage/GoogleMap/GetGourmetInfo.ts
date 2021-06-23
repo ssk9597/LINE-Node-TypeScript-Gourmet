efe4257b348ac5619d7c2fd2434dc5d6b1515fda
sasaki <skyo.97.admj@gmail.com>
2021-06-23 21:53:22 +0900

    feature/Update_env

--- api/controller/src/Common/TemplateMessage/GoogleMap/GetGourmetInfo.ts
+++ api/controller/src/Common/TemplateMessage/GoogleMap/GetGourmetInfo.ts
@@ -4,7 +4,7 @@ import axios, { AxiosResponse } from 'axios';
 export const getGourmetInfo = async (latitude: number, longitude: number) => {
   try {
     // Google Map API
-    const Google_API = process.env.GOOGLE_DEV_API;
+    const Google_API = process.env.GOOGLE_PROD_API;
 
     const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=800&type=restaurant&key=${Google_API}&language=ja`;
     const gourmets: AxiosResponse<any> = await axios.get(url);
