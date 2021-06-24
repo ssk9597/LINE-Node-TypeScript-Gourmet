9a2705430abd12f7d723ba5919b64480278277db
sasaki <skyo.97.admj@gmail.com>
2021-06-24 11:43:11 +0900

    Update Types

--- api/controller/src/Common/TemplateMessage/GoogleMap/type/SortRatingGourmet.type.ts
+++ api/controller/src/Common/TemplateMessage/GoogleMap/type/SortRatingGourmet.type.ts
@@ -7,11 +7,13 @@ export type GourmetData = {
   vicinity: string;
 };
 
-export type SortGourmetData = {
-  geometry_location_lat: number;
-  geometry_location_lng: number;
-  name: string;
-  photo_reference: string;
-  rating: number;
-  vicinity: string;
-}[];
+export type SortGourmetData = GourmetData[];
+
+// {
+//   geometry_location_lat: number;
+//   geometry_location_lng: number;
+//   name: string;
+//   photo_reference: string;
+//   rating: number;
+//   vicinity: string;
+// }[];
